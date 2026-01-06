48ea2424eee10a2d7afd3bd90f84f9bcd41a8a84
Manav Lohar <manavlohar414141@gmail.com>
2026-01-06 08:17:16 -0800

    Setup1 for vercel hosting.
    Signed-off-by: Manav Lohar <manavlohar414141@gmail.com>

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true;
     console.log(`Database connected ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error: ", error.message);
     process.exit(1);
   }
 };
+
+export { isConnected };
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { connectDB } from "./db/index.js";
+// import { connectDB, isConnected } from "./db/index.js";
 import { adminRouter } from "./router/admin.router.js";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
@@ -19,7 +19,29 @@ app.use(
 app.use(cookieParser());
 app.use("/admin", adminRouter);
 
-connectDB();
-app.listen(port, () => {
-  console.log(`Server Started on port: ${port}: ${uuidv4()}`);
+let isConnected = false;
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true;
+    console.log(`Database connected ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error: ", error.message);
+    process.exit(1);
+  }
+};
+
+app.use((req, res, next) => {
+  if (!isConnected) {
+    connectDB();
+  }
+  next();
 });
+
+// connectDB();
+// app.listen(port, () => {
+//   console.log(`Server Started on port: ${port}: ${uuidv4()}`);
+// });
+
+export { app };
